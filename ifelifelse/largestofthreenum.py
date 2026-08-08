3c7596d5fc3b703bd445a45fa6f95ce8ef4dcce2
vinnumuthyala1025-lab <vinnumuthyala1025@gmail.com>
2026-08-08 16:53:57 +0530

    2

---
+++ ifelifelse/largestofthreenum.py
@@ -0,0 +1,13 @@
+a = int(input('Enter the number:'))
+b  = int(input("enter the number:"))
+c = int(input("enter the number:"))
+
+if a>=b and a>=c:
+    print(a,"is the largest ")
+
+elif b>=a and b>=c:
+    print(b,"is largest")
+
+else :
+    print("c is the greatest")
+

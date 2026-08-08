cdf1568850089cf1f78e6d8b2c7933731569be86
vinnumuthyala1025-lab <vinnumuthyala1025@gmail.com>
2026-08-08 16:58:08 +0530

    uploading 2

---
+++ type/ty.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+print(name)
+sec = input("What is your section? ")
+print(sec)
+
+num1 =int(input("Enter the number:"))
+num2 =int(input("Enter the number:"))
+
+print(type(num1))
+print(type(num2))
+print(type(name))
+print(type(sec))
